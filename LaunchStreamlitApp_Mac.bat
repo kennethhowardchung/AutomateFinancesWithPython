44437d6ffb471fe58a8d52b1a79edd0c03344cd7
Kenneth Personal Com <kennethhowardchung@gmail.com>
2025-04-30 00:35:58 +0800

    .

--- LaunchStreamlitApp_Mac.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Navigate to project directory
-cd /d "C:\Users\KennethChung\OneDrive - fenghe asia\Desktop\Python\Automate_Finance_Proj\AutomateFinancesWithPython"
-
-REM Activate your virtual environment
-call C:\envs\myenv\Scripts\activate.bat
-
-REM Launch Streamlit app
-streamlit run main.py
